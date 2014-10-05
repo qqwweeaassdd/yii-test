cd34611b374a6d6b6c324017ee1332e1610b1725
qqwweeaassdd <reg_inbox@inbox.ru>
2014-10-05 14:34:25 +0600

    + db migration

---
+++ web/migrations/m141005_075112_create_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141005_075112_create_user_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('registration', [
+            'email'  => Schema::TYPE_STRING . ' NOT NULL PRIMARY KEY',
+            'secure' => Schema::TYPE_STRING,
+            'used'   => Schema::TYPE_BOOLEAN,
+        ]);
+
+        $this->createTable('user', [
+            'email' => Schema::TYPE_STRING . ' NOT NULL PRIMARY KEY',
+            'name'  => Schema::TYPE_STRING,
+        ]);
+
+        $this->createIndex('email', 'registration', 'email', true);
+        $this->createIndex('email', 'user', 'email', true);
+    }
+
+    public function down()
+    {
+        $this->dropTable('registration');
+        $this->dropTable('user');
+    }
+}
